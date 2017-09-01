d58d63366824a966a251e3e43a90b92a009eaed1
chaiyu <chaiyu2002@gmail.com>
2017-09-01 22:22:35 +0800

    see #855_4

--- src/main/java/com/xmheart/controller/MainController.java
+++ src/main/java/com/xmheart/controller/MainController.java
@@ -1,26 +1,60 @@
 package com.xmheart.controller;
-
+ 
+import java.util.ArrayList;
 import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
+ 
+import com.xmheart.form.PersonForm;
+import com.xmheart.model.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.xmheart.model.Department;
-import com.xmheart.model.DepartmentService;
  
 @Controller
 public class MainController {
  
-   @Autowired
-   private DepartmentService deptService;
+    private static List<Person> persons = new ArrayList<Person>();
  
-   @RequestMapping(value = { "/", "/welcome" }, method = RequestMethod.GET)
-   public String welcomePage(Model model) {
-       List<Department> list = deptService.listDepartment();
-       model.addAttribute("departments", list);
-       return "index";
-   }
+    static {
+        persons.add(new Person("Bill", "Gates"));
+        persons.add(new Person("Steve", "Jobs"));
+    }
+ 
+    @RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
+    public String index(Model model) {
+ 
+        model.addAttribute("persons", persons);
+ 
+        return "index";
+    }
+ 
+    @RequestMapping(value = { "/addPerson" }, method = RequestMethod.GET)
+    public String addPersonForm(Model model) {
+ 
+        PersonForm personForm = new PersonForm();
+        model.addAttribute("personForm", personForm);
+ 
+        return "addPerson";
+    }
+ 
+    @RequestMapping(value = { "/addPerson" }, method = RequestMethod.POST)
+    public String addPersonSave(Model model, //
+            @ModelAttribute("personForm") PersonForm personForm) {
+ 
+        String firstName = personForm.getFirstName();
+        String lastName = personForm.getLastName();
+ 
+        if (firstName != null && firstName.length() > 0 //
+                && lastName != null && lastName.length() > 0) {
+            Person newPerson = new Person(firstName, lastName);
+            persons.add(newPerson);
+ 
+            return "redirect:/index";
+        }
+        String error = "First Name & Last Name is required!";
+        model.addAttribute("errorMessage", error);
+        return "addPerson";
+    }
+ 
 }
