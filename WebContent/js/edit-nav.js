6e74d2b201e89453918f5b68e6f082248fbfa54d
zch <zch@zchdeMacBook-Pro.local>
2017-09-23 00:56:17 +0800

    FIX: 修改时间

--- WebContent/js/edit-nav.js
+++ WebContent/js/edit-nav.js
@@ -37,13 +37,49 @@ exports.XPW.NavEdit = (function() {
       data: {columnId: val}
     })
     .done(function(data) {
+    		$.each(data, function(name, val){
+    	   	  val.publishTime = NavEdit._dateFilter(val.publishTime)
+    	    })
     		var secondColumnTemplate = $('#secondColumnTd').html();
     	    Mustache.parse(secondColumnTemplate);   // optional, speeds up future uses
     	    var rendered = Mustache.render(secondColumnTemplate, {data});
     	    $('#secondTable').html(rendered);
     })
   }
+  
+  NavEdit._dateFilter = function (date, formatString) {
+	  if (!date) return ''
+	  var date = new Date(date)
 
+	  formatString = formatString || 'yyyy-MM-dd'
+
+	  var dateMap = {
+	    year: date.getFullYear(),
+	    month: date.getMonth() + 1,
+	    day: date.getDate(),
+	    hour: date.getHours(),
+	    minutes: date.getMinutes(),
+	    seconds: date.getSeconds()
+	  }
+
+	  for (var key in dateMap) {
+	    let value = dateMap[key]
+	    value = value < 10 ? ('0' + value) : value
+	    dateMap[key] = value.toString()
+	  }
+
+	  const { year, month, day, hour, minutes, seconds } = dateMap
+
+	  const formatDate = formatString
+	    .replace(/y+/, ($0) => (year.substring(year.length, -$0.length)))
+	    .replace(/M+/, () => month)
+	    .replace(/d+/, () => day)
+	    .replace(/H+/, () => hour)
+	    .replace(/h+/, () => hour % 12 === 0 ? 12 : hour % 12)
+	    .replace(/m+/, () => minutes)
+	    .replace(/s+/, () => seconds)
+	  return formatDate
+	}
  
 
   return NavEdit;
