e44bd7d6566851cbf126c57af57e8e1651e7aa86
zch <zch@zchdeMacBook-Pro.local>
2017-10-10 00:17:15 +0800

    FEAT: 首页轮播编辑

---
+++ WebContent/js/edit-index.js
@@ -0,0 +1,34 @@
+exports = this;
+exports.XPW = exports.EDIT || {};
+exports.XPW.IndexEdit = (function() {
+  function IndexEdit() {
+	  // 初始化页面处理。
+	  IndexEdit.addSlideImg();
+  }
+  
+  IndexEdit.addSlideImg = function () {
+	  $('.add-list-main').on('click', '#addImgBtn', function (){
+		  $(this).siblings('.upload-form').find('.add-img-file').trigger('click');
+	  })
+	  $('.add-list-main').on('change', '.add-img-file', function (){
+		  $(this).siblings('.add-img-submit').trigger('click');
+	  })
+	  $('#uploadForm').submit(function(){
+		  $this = $(this);
+		  $this.ajaxSubmit({
+			  success: function (responseText) {
+				  var img = responseText;
+				  $this.siblings('.add-image-url').find('.upload-img').attr('src', img);
+				  $this.siblings('.add-image-button').hide();
+				  $this.siblings('.add-image-url').show();
+			  }
+		  });
+		  return false;
+	  })
+  }
+  
+  
+
+
+  return IndexEdit;
+})();
