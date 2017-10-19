c1f69868e8b550b2b6e8ddcb454da94a2e3dbe85
cancan <790864964@qq.com>
2017-10-19 23:44:11 +0800

    :sparkles: 登录

--- WebContent/js/http-intercept.js
+++ WebContent/js/http-intercept.js
@@ -16,7 +16,7 @@ $(function () {
         window.location.replace(url);
     }
 
-    // 在接受到数据后做统一处理s
+    // 在接收到数据后做统一处理
     $(document).ajaxError(function (event, request, settings) {
         if (request.status == 401) {
             var url = 'http://' + window.location.host + '/login.html'
