0243a7e6123ec3d431977a5d0d6730af949acc2a
chaiyu <chaiyu2002@gmail.com>
2017-10-22 23:38:11 +0800

    see #1084_2

--- src/main/java/com/xmheart/backend/controller/VideoController.java
+++ src/main/java/com/xmheart/backend/controller/VideoController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,7 +22,6 @@ public class VideoController {
     @Autowired
     VideoService videoService;
 
-    // TODO 这个接口要移到后台系统中去
     @ApiOperation(value = "创建一个视频", notes = "创建一篇视频")
     @RequestMapping(value = { "/videos" }, method = RequestMethod.POST)
     public ResponseEntity<?> create(@ApiParam("视频标题") @RequestParam String title,
@@ -53,6 +53,45 @@ public class VideoController {
         if (ret > 0) {
             video.setUrl("/videoNewsDetail?id=" + String.valueOf(video.getId()));
             videoService.update(video);
+            return ResponseEntity.ok(null);
+        } else {
+            return ResponseEntity.status(HttpServletResponse.SC_INTERNAL_SERVER_ERROR).body(null);
+        }
+
+    }
+    
+    @ApiOperation(value = "创建一个视频", notes = "创建一篇视频")
+    @RequestMapping(value = { "/videos/{id}" }, method = RequestMethod.POST)
+    public ResponseEntity<?> update(@ApiParam("视频Id") @PathVariable Long id,
+            @ApiParam("视频标题") @RequestParam(required = false) String title,
+            @ApiParam("视频缩略图的地址") @RequestParam(required = false) String imgUrl,
+            @ApiParam("视频地址") @RequestParam(required = false) String videoUrl,
+            @ApiParam("视频描述") @RequestParam(required = false) String brief,
+            @ApiParam("是否置顶") @RequestParam(required = false) Boolean isPinned) {
+        XPWVideo video = new XPWVideo();
+        
+        video.setId(id);
+
+        video.setTitle(title);
+
+        if (imgUrl != null) {
+            video.setUrl(imgUrl);
+        }
+
+        if (videoUrl != null) {
+            video.setImgUrl(videoUrl);
+        }
+
+        if (brief != null) {
+            video.setBrief(brief);
+        }
+
+        if (isPinned != null) {
+            video.setIsPinned(isPinned);
+        }
+
+        int ret = videoService.update(video);
+        if (ret > 0) {
             return ResponseEntity.ok(null);
         } else {
             return ResponseEntity.status(HttpServletResponse.SC_INTERNAL_SERVER_ERROR).body(null);
