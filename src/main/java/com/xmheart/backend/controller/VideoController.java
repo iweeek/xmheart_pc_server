980aea174477e445089fa861cfe53491ea47e1ad
chaiyu <chaiyu2002@gmail.com>
2017-10-23 18:25:37 +0800

    see #944_3

--- src/main/java/com/xmheart/backend/controller/VideoController.java
+++ src/main/java/com/xmheart/backend/controller/VideoController.java
@@ -1,5 +1,9 @@
 package com.xmheart.backend.controller;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
 import com.xmheart.model.XPWVideo;
 import com.xmheart.service.VideoService;
 import io.swagger.annotations.Api;
@@ -21,25 +26,50 @@ public class VideoController {
 
     @Autowired
     VideoService videoService;
+    
+    @ApiOperation(value = "获取视频列表", notes = "获取视频列表")
+    @RequestMapping(value = { "/videos" }, method = RequestMethod.GET)
+    public ResponseEntity<?> index() {
+        List<XPWVideo> list = new ArrayList<XPWVideo>();
 
-    @ApiOperation(value = "创建一个视频", notes = "创建一篇视频")
+        list = videoService.index();
+        if (list.size() > 0) {
+            return ResponseEntity.ok(list);
+        } else {
+            return ResponseEntity.status(HttpServletResponse.SC_NOT_FOUND).body(null);
+        }
+    }
+    
+    @ApiOperation(value = "获取院报列表", notes = "获取院报列表")
+    @RequestMapping(value = { "/videos/{id}" }, method = RequestMethod.GET)
+    public ResponseEntity<?> read(@PathVariable Long id) {
+        XPWVideo video = videoService.read(id);
+        if (video != null) {
+            return ResponseEntity.ok(video);
+        } else {
+            return ResponseEntity.status(HttpServletResponse.SC_NOT_FOUND).body(null);
+        }
+    }    
+
+    @ApiOperation(value = "创建一个视频", notes = "创建一个视频")
     @RequestMapping(value = { "/videos" }, method = RequestMethod.POST)
     public ResponseEntity<?> create(@ApiParam("视频标题") @RequestParam String title,
             @ApiParam("视频缩略图的地址") @RequestParam(required = false) String imgUrl,
             @ApiParam("视频地址") @RequestParam(required = false) String videoUrl,
             @ApiParam("视频描述") @RequestParam(required = false) String brief,
             @ApiParam("是否置顶") @RequestParam(required = false) Boolean isPinned,
-            @ApiParam("图片地址") @RequestParam(required = false) Boolean isPublished) {
+            @ApiParam("图片地址") @RequestParam(required = false) Boolean isPublished,
+            @ApiParam("发布时间") @RequestParam(required = false) String publishTime) {
         XPWVideo video = new XPWVideo();
 
         video.setTitle(title);
 
         if (imgUrl != null) {
-            video.setUrl(imgUrl);
+            video.setImgUrl(imgUrl);
         }
 
         if (videoUrl != null) {
-            video.setImgUrl(videoUrl);
+            video.setVideoUrl(videoUrl);
         }
 
         if (brief != null) {
@@ -53,16 +83,23 @@ public class VideoController {
         if (isPublished != null) {
             video.setIsPublished(isPublished);
         }
+        
+        if (publishTime != null) {
+            video.setPublishTime(new Date(publishTime));
+        } else {
+            video.setPublishTime(new Date());
+        }
+       
+        video.setUrl("");
 
         int ret = videoService.create(video);
         if (ret > 0) {
             video.setUrl("/videoNewsDetail?id=" + String.valueOf(video.getId()));
             videoService.update(video);
-            return ResponseEntity.ok(null);
+            return ResponseEntity.status(HttpServletResponse.SC_CREATED).body(video);
         } else {
             return ResponseEntity.status(HttpServletResponse.SC_INTERNAL_SERVER_ERROR).body(null);
         }
-
     }
     
     @ApiOperation(value = "更新一个视频", notes = "更新一篇视频")
